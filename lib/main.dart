a87eaf25161bd4930c9ea644ba1156d17007c348
hwangtaewook <87569211+hwangtaewook@users.noreply.github.com>
2024-05-09 20:13:03 +0900

    feat : BottomNavigationBar 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:view_finder/presentation/tab_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: TabPage(),
     );
   }
 }
