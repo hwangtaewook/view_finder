cb2287b91aa3e935e23a50ff1626c01e2e2fc29c
hwangtaewook <hwangxodnr@nate.com>
2024-05-11 18:11:14 +0900

    faet : PhotoScreen UI 제작

--- lib/presentation/photo/photo_screen.dart
+++ lib/presentation/photo/photo_screen.dart
@@ -1,10 +1,93 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class PhotoScreen extends StatelessWidget {
   const PhotoScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final List<String> catPics = [
+      'assets/1.jpg',
+      'assets/2.jpg',
+      'assets/3.jpg',
+      'assets/4.jpg',
+      'assets/5.jpg',
+      'assets/6.jpg',
+      'assets/7.jpg',
+      'assets/8.jpg',
+      'assets/9.jpg',
+      'assets/10.jpg',
+      'assets/11.jpg',
+    ];
+
+    return SafeArea(
+      child: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            toolbarHeight: 0.12.sw,
+            title: Stack(
+              children: [
+                ClipOval(
+                  child: Image.asset(
+                    height: 0.08.sw,
+                    width: 0.08.sw,
+                    'assets/back2.jpg',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Center(
+                  child: Text(
+                    'Photo',
+                    style: TextStyle(fontSize: 18.sp),
+                  ),
+                ),
+              ],
+            ),
+            pinned: true,
+            scrolledUnderElevation: 0,
+          ),
+          SliverPadding(
+            padding: const EdgeInsets.all(16),
+            sliver: SliverMasonryGrid.count(
+              crossAxisCount: 2,
+              mainAxisSpacing: 10,
+              crossAxisSpacing: 10,
+              itemBuilder: (BuildContext context, int index) {
+                final int firstItemIndex = index * 2;
+                final int secondItemIndex = firstItemIndex + 1;
+                final List<String> catPics = [
+                  'assets/1.jpg',
+                  'assets/2.jpg',
+                  'assets/3.jpg',
+                  'assets/4.jpg',
+                  'assets/5.jpg',
+                  'assets/6.jpg',
+                  'assets/7.jpg',
+                  'assets/8.jpg',
+                  'assets/9.jpg',
+                  'assets/10.jpg',
+                  'assets/11.jpg',
+                ];
+
+                return ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.asset(
+                    catPics[index % catPics.length],
+                    fit: BoxFit.contain,
+                  ),
+                );
+              },
+              childCount: 30,
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: SizedBox(
+              height: 0.1.sh,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
