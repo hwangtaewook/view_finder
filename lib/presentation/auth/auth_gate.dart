dbdf803b936e455f1e02b42dfbd6ec081d4e6ee8
hwangtaewook <87569211+hwangtaewook@users.noreply.github.com>
2024-05-14 00:57:49 +0900

    chore : screen -> page로 이름 변경

--- lib/presentation/auth/auth_gate.dart
+++ lib/presentation/auth/auth_gate.dart
@@ -2,7 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 
-import '../nav/nav_bar.dart';
+import '../nav/nav_bar_screen.dart';
 
 class AuthGate extends StatelessWidget {
   const AuthGate({super.key});
@@ -18,9 +18,12 @@ class AuthGate extends StatelessWidget {
             providers: [
               EmailAuthProvider(),
             ],
+            headerBuilder: (context, constraints, _) {
+              return Text('asd');
+            },
           );
         }
-        return const NavBar();
+        return const NavBarScreen();
       },
     );
   }
