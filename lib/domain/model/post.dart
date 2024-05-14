e8b214e33089d79b5d6fd1d3076277dac010cf8c
hwangtaewook <87569211+hwangtaewook@users.noreply.github.com>
2024-05-14 16:58:45 +0900

    feat : post model 생성

---
+++ lib/domain/model/post.dart
@@ -0,0 +1,17 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'post.freezed.dart';
+
+part 'post.g.dart';
+
+@freezed
+class Post with _$Post {
+  const factory Post({
+    required String userId,
+    required String title,
+    required String detail,
+    required String imageUrl,
+  }) = _Post;
+
+  factory Post.fromJson(Map<String, Object?> json) => _$PostFromJson(json);
+}
