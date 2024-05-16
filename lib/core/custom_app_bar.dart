4dc672583518853152fbc5da971a9a37385be3e0
hwangtaewook <87569211+hwangtaewook@users.noreply.github.com>
2024-05-16 17:49:39 +0900

    refactor : CustomAppBar 수정

--- lib/core/custom_app_bar.dart
+++ lib/core/custom_app_bar.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -13,28 +14,21 @@ class CustomAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SliverAppBar(
+      centerTitle: true,
       toolbarHeight: 0.055.sh,
-      title: Stack(
-        alignment: Alignment.center,
-        children: [
-          Align(
-            alignment: Alignment.centerLeft,
-            child: ClipOval(
-              child: Image.asset(
-                height: 0.08.sw,
-                width: 0.08.sw,
-                userImageURL,
-                fit: BoxFit.cover,
-              ),
-            ),
+      title: Text(
+        screenName,
+        style: TextStyle(fontSize: 18.sp),
+      ),
+      leading: Center(
+        child: ClipOval(
+          child: Image.network(
+            height: 0.08.sw,
+            width: 0.08.sw,
+            userImageURL,
+            fit: BoxFit.cover,
           ),
-          Center(
-            child: Text(
-              screenName,
-              style: TextStyle(fontSize: 18.sp),
-            ),
-          ),
-        ],
+        ),
       ),
       pinned: true,
       scrolledUnderElevation: 0,
