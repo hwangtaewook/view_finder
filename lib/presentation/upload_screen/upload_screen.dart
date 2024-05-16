9e0c9b949d5aea74409b8ee5f30da869546b16c5
hwangtaewook <87569211+hwangtaewook@users.noreply.github.com>
2024-05-16 16:47:38 +0900

    chore : CircularProgressIndicator 수정

--- lib/presentation/upload_screen/upload_screen.dart
+++ lib/presentation/upload_screen/upload_screen.dart
@@ -30,113 +30,113 @@ class _UploadScreenState extends State<UploadScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            centerTitle: true,
-            // navpush로 생긴 back 버튼에 밀림 없이 센터로 정렬
-            toolbarHeight: 0.055.sh,
-            title: Text(
-              'Upload',
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 18.sp),
-            ),
-            pinned: true,
-            scrolledUnderElevation: 0,
-            actions: [
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 0.01.sh),
-                child: GestureDetector(
-                  onTap: () async {
-                    if (_image != null &&
-                        _titleTextEditingController.text.isNotEmpty &&
-                        _detailTextEditingController.text.isNotEmpty) {
-                      setState(() {
-                        isLoading = true;
-                      });
+      body: isLoading
+          ? Center(child: const CircularProgressIndicator())
+          : CustomScrollView(
+              slivers: [
+                SliverAppBar(
+                  centerTitle: true,
+                  // navpush로 생긴 back 버튼에 밀림 없이 센터로 정렬
+                  toolbarHeight: 0.055.sh,
+                  title: Text(
+                    'Upload',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 18.sp),
+                  ),
+                  pinned: true,
+                  scrolledUnderElevation: 0,
+                  actions: [
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 0.01.sh),
+                      child: GestureDetector(
+                        onTap: () async {
+                          if (_image != null &&
+                              _titleTextEditingController.text.isNotEmpty &&
+                              _detailTextEditingController.text.isNotEmpty) {
+                            setState(() {
+                              isLoading = true;
+                            });
 
-                      await viewModel.uploadPost(
-                        _titleTextEditingController.text,
-                        _detailTextEditingController.text,
-                        _image!,
-                      );
+                            await viewModel.uploadPost(
+                              _titleTextEditingController.text,
+                              _detailTextEditingController.text,
+                              _image!,
+                            );
 
-                      setState(() {
-                        isLoading = false;
-                      });
+                            setState(() {
+                              isLoading = false;
+                            });
 
-                      if (context.mounted) {
-                        Navigator.pop(context);
-                      }
-                    }
-                  },
-                  child: const Icon(
-                    Icons.send,
+                            if (context.mounted) {
+                              Navigator.pop(context);
+                            }
+                          }
+                        },
+                        child: const Icon(
+                          Icons.send,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                if (_image == null)
+                  SliverToBoxAdapter(
+                    child: Icon(
+                      Icons.add_photo_alternate_outlined,
+                      size: 0.3.sh,
+                    ),
+                  ),
+                if (_image != null)
+                  SliverToBoxAdapter(
+                    child: Image.file(_image!),
+                  ),
+                SliverToBoxAdapter(
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      _image = await viewModel.getImage();
+                      setState(() {});
+                    },
+                    child: const Text('이미지 선택'),
                   ),
                 ),
-              ),
-            ],
-          ),
-          if (_image == null)
-            SliverToBoxAdapter(
-              child: Icon(
-                Icons.add_photo_alternate_outlined,
-                size: 0.3.sh,
-              ),
-            ),
-          if (_image != null)
-            SliverToBoxAdapter(
-              child: isLoading
-                  ? const CircularProgressIndicator()
-                  : Image.file(_image!),
-            ),
-          SliverToBoxAdapter(
-            child: ElevatedButton(
-              onPressed: () async {
-                _image = await viewModel.getImage();
-                setState(() {});
-              },
-              child: const Text('이미지 선택'),
-            ),
-          ),
-          SliverToBoxAdapter(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                children: [
-                  TextField(
-                    controller: _titleTextEditingController,
-                    onTapOutside: (event) =>
-                        FocusManager.instance.primaryFocus?.unfocus(),
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
+                SliverToBoxAdapter(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      children: [
+                        TextField(
+                          controller: _titleTextEditingController,
+                          onTapOutside: (event) =>
+                              FocusManager.instance.primaryFocus?.unfocus(),
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              filled: true,
+                              hintText: '제목을 입력하세요',
+                              fillColor: Colors.white70),
                         ),
-                        filled: true,
-                        hintText: '제목을 입력하세요',
-                        fillColor: Colors.white70),
-                  ),
-                  SizedBox(
-                    height: 0.1.sh,
-                  ),
-                  TextField(
-                    controller: _detailTextEditingController,
-                    onTapOutside: (event) =>
-                        FocusManager.instance.primaryFocus?.unfocus(),
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
+                        SizedBox(
+                          height: 0.1.sh,
                         ),
-                        filled: true,
-                        hintText: '내용을 입력하세요',
-                        fillColor: Colors.white70),
+                        TextField(
+                          controller: _detailTextEditingController,
+                          onTapOutside: (event) =>
+                              FocusManager.instance.primaryFocus?.unfocus(),
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              filled: true,
+                              hintText: '내용을 입력하세요',
+                              fillColor: Colors.white70),
+                        ),
+                      ],
+                    ),
                   ),
-                ],
-              ),
+                )
+              ],
             ),
-          )
-        ],
-      ),
     );
   }
 }
