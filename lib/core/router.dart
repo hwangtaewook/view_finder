a76fc06b93cfa97d85a0e09fc6e95f850dd226ee
hwangtaewook <87569211+hwangtaewook@users.noreply.github.com>
2024-05-18 13:09:52 +0900

    feat : gorouter 추가

---
+++ lib/core/router.dart
@@ -0,0 +1,47 @@
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:go_router/go_router.dart';
+import 'package:view_finder/presentation/nav_screen/nav_bar_screen.dart';
+
+final router = GoRouter(
+  initialLocation: '/sign_in',
+  routes: [
+    GoRoute(
+      path: '/sign_in',
+      builder: (context, state) {
+        return SignInScreen(
+          providers: [
+            EmailAuthProvider(),
+          ],
+        );
+      },
+      routes: [
+        GoRoute(
+          path: 'forgot_password',
+          builder: (context, state) {
+            return const ForgotPasswordScreen();
+          },
+        ),
+      ],
+    ),
+    GoRoute(
+      path: '/nav_bar',
+      builder: (context, state) {
+        return const NavBarScreen();
+      },
+      routes: [
+        GoRoute(
+          path: 'upload',
+          builder: (context, state) {
+            return const ForgotPasswordScreen();
+          },
+        ),
+        GoRoute(
+          path: 'detail_post',
+          builder: (context, state) {
+            return const ForgotPasswordScreen();
+          },
+        ),
+      ],
+    ),
+  ],
+);
