92f180f933d59d52d823046185beebcae047fd9a
hwangtaewook <87569211+hwangtaewook@users.noreply.github.com>
2024-05-19 22:38:14 +0900

    fix : 라우터 수정

--- lib/core/router.dart
+++ lib/core/router.dart
@@ -1,7 +1,10 @@
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:go_router/go_router.dart';
 import 'package:view_finder/presentation/auth/auth_gate.dart';
+import 'package:view_finder/presentation/detail_post_screen/detail_post_screen.dart';
 import 'package:view_finder/presentation/nav_screen/nav_bar_screen.dart';
+import 'package:view_finder/presentation/upload_screen/upload_screen.dart';
+import '../domain/model/post.dart';
 
 final router = GoRouter(
   initialLocation: '/sign_in',
@@ -29,13 +32,14 @@ final router = GoRouter(
         GoRoute(
           path: 'upload',
           builder: (context, state) {
-            return const ForgotPasswordScreen();
+            return const UploadScreen();
           },
         ),
         GoRoute(
           path: 'detail_post',
           builder: (context, state) {
-            return const ForgotPasswordScreen();
+            final post = state.extra as Post;
+            return DetailPostScreen(post: post);
           },
         ),
       ],
