0576fdd02e5cfa9baa96eeec993b1f5fc7cb9466
hwangtaewook <87569211+hwangtaewook@users.noreply.github.com>
2024-05-20 16:06:18 +0900

    move : 파일 위치 변경

---
+++ lib/presentation/nav_screen/component/image_card.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class ImageCard extends StatelessWidget {
+  final String image;
+  final String userImage;
+  final String userName;
+
+  const ImageCard({
+    super.key,
+    required this.image,
+    required this.userImage,
+    required this.userName,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 1.sw,
+      child: Column(
+        children: [
+          Column(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.network(image),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 0.02.sh),
+                child: Row(
+                  children: [
+                    ClipOval(
+                      child: Image.network(
+                        height: 0.08.sw,
+                        width: 0.08.sw,
+                        userImage,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 0.05.sw,
+                    ),
+                    Text(
+                      userName,
+                      style: TextStyle(fontSize: 18.sp),
+                    ),
+                    const Spacer(),
+                    const Icon(Icons.star_outline),
+                    const Icon(Icons.comment_outlined),
+                  ],
+                ),
+              ),
+              SizedBox(height: 0.03.sh)
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
