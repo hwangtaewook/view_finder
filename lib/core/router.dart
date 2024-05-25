e295389dc669378560adbc5a7c34313c9e246045
hwangtaewook <87569211+hwangtaewook@users.noreply.github.com>
2024-05-25 12:43:06 +0900

    feat : router에 ViewModel 삽입

--- lib/core/router.dart
+++ lib/core/router.dart
@@ -1,10 +1,17 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
 import 'package:view_finder/presentation/auth/auth_gate.dart';
 import 'package:view_finder/presentation/detail_post_screen/detail_post_screen.dart';
+import 'package:view_finder/presentation/nav_screen/account/account_page.dart';
 import 'package:view_finder/presentation/nav_screen/nav_bar_screen.dart';
 import 'package:view_finder/presentation/upload_screen/upload_screen.dart';
+import '../data/data_source/post_data_source.dart';
+import '../data/repository/post_repository_impl.dart';
 import '../domain/model/post.dart';
+import '../presentation/nav_screen/account/account_view_model.dart';
+import '../presentation/nav_screen/nav_bar_view_model.dart';
 
 final router = GoRouter(
   initialLocation: '/sign_in',
@@ -26,7 +33,30 @@ final router = GoRouter(
     GoRoute(
       path: '/nav_bar',
       builder: (context, state) {
-        return const NavBarScreen();
+        return MultiProvider(
+          providers: [
+            ChangeNotifierProvider(
+              create: (context) => NavBarViewModel(
+                postRepository: PostRepositoryImpl(
+                  postDataSource: PostDataSource(
+                    firebaseFirestore: FirebaseFirestore.instance,
+                  ),
+                ),
+              ),
+              child: const NavBarScreen(),
+            ),
+            ChangeNotifierProvider(
+              create: (context) => AccountViewModel(
+                postRepository: PostRepositoryImpl(
+                  postDataSource: PostDataSource(
+                    firebaseFirestore: FirebaseFirestore.instance,
+                  ),
+                ),
+              ),
+              child: const AccountPage(),
+            ),
+          ],
+        );
       },
       routes: [
         GoRoute(
