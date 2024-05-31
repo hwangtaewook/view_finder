2297bd95e22086eef0850497d25a4e6dc57c0d29
hwangtaewook <87569211+hwangtaewook@users.noreply.github.com>
2024-05-31 16:37:55 +0900

    feat: member view model 연동 및 파이어베이스에 데이터 넘겨주기

--- lib/presentation/auth/memeber_detail_screen.dart
+++ lib/presentation/auth/memeber_detail_screen.dart
@@ -1,19 +1,50 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
 import 'package:view_finder/presentation/auth/component/custom_text_filed.dart';
+import 'member_detail_view_model.dart';
 
-class MemberDetailScreen extends StatelessWidget {
+class MemberDetailScreen extends StatefulWidget {
+  const MemberDetailScreen({super.key});
+
+  @override
+  State<MemberDetailScreen> createState() => _MemberDetailScreenState();
+}
+
+class _MemberDetailScreenState extends State<MemberDetailScreen> {
   File? _image;
+
   final _lastNameTextEditingController = TextEditingController();
+
   final _firstNameTextEditingController = TextEditingController();
+
   final _nickNameTextEditingController = TextEditingController();
+
   final _emailTextEditingController = TextEditingController();
 
-  MemberDetailScreen({super.key});
+  @override
+  void initState() {
+    super.initState();
+    Future.microtask(() {
+      context.read<MemberDetailViewModel>();
+    });
+  }
+
+  @override
+  void dispose() {
+    // 추가된 부분: 컨트롤러 정리
+    _lastNameTextEditingController.dispose();
+    _firstNameTextEditingController.dispose();
+    _nickNameTextEditingController.dispose();
+    _emailTextEditingController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
+    final viewModel = context.watch<MemberDetailViewModel>();
+
     return SafeArea(
       child: Scaffold(
         body: Padding(
@@ -34,8 +65,8 @@ class MemberDetailScreen extends StatelessWidget {
                     ),
                     CustomTextFiled(
                       controller: _firstNameTextEditingController,
-                      fieldName: '닉네임',
-                      hintText: '닉네임을 입력하세요',
+                      fieldName: '이름',
+                      hintText: '이름을 입력하세요',
                     ),
                   ],
                 ),
@@ -43,9 +74,9 @@ class MemberDetailScreen extends StatelessWidget {
               SliverToBoxAdapter(child: SizedBox(height: 0.06.sw)),
               SliverToBoxAdapter(
                 child: CustomTextFiled(
-                  controller: _firstNameTextEditingController,
-                  fieldName: '이름',
-                  hintText: '이름을 입력하세요',
+                  controller: _nickNameTextEditingController,
+                  fieldName: '닉네임',
+                  hintText: '닉네임을 입력하세요',
                 ),
               ),
               SliverToBoxAdapter(child: SizedBox(height: 0.06.sw)),
@@ -57,22 +88,28 @@ class MemberDetailScreen extends StatelessWidget {
                 ),
               ),
               SliverToBoxAdapter(child: SizedBox(height: 0.06.sw)),
-              if (_image == null)
-                SliverToBoxAdapter(
-                  child: Icon(
-                    Icons.add_photo_alternate_outlined,
-                    size: 0.3.sh,
+              SliverToBoxAdapter(
+                child: Center(
+                  child: SizedBox(
+                    height: 0.3.sh,
+                    width: 0.3.sh,
+                    child: _image == null
+                        ? Icon(
+                            Icons.add_photo_alternate_outlined,
+                            size: 0.3.sh,
+                          )
+                        : Image.file(
+                            _image!,
+                            fit: BoxFit.cover,
+                          ),
                   ),
                 ),
-              if (_image != null)
-                SliverToBoxAdapter(
-                  child: Image.file(_image!),
-                ),
+              ),
               SliverToBoxAdapter(
                 child: ElevatedButton(
                   onPressed: () async {
-                    // _image = await viewModel.getImage();
-                    // setState(() {});
+                    _image = await viewModel.getImage();
+                    setState(() {});
                   },
                   child: const Text('프로필 이미지 선택'),
                 ),
@@ -81,8 +118,11 @@ class MemberDetailScreen extends StatelessWidget {
               SliverToBoxAdapter(
                 child: ElevatedButton(
                   onPressed: () async {
-                    // _image = await viewModel.getImage();
-                    // setState(() {});
+                    viewModel.uploadMember(
+                        _lastNameTextEditingController.text,
+                        _firstNameTextEditingController.text,
+                        _nickNameTextEditingController.text,
+                        _image!);
                   },
                   child: const Text('완료'),
                 ),
