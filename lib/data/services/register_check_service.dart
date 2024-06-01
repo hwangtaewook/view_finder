6a85d72737cbe7ae764f884b773fbd30ad198431
hwangtaewook <87569211+hwangtaewook@users.noreply.github.com>
2024-06-01 13:26:56 +0900

    feat : 등록 체크 서비스 추가

---
+++ lib/data/services/register_check_service.dart
@@ -0,0 +1,21 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:view_finder/data/data_source/member_data_source.dart';
+
+class RegisterCheckService {
+  final MemberDataSource _memberDataSource;
+
+  const RegisterCheckService({
+    required MemberDataSource memberDataSource,
+  }) : _memberDataSource = memberDataSource;
+
+  Future<bool> registerCheck() async {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user == null || user.uid.isEmpty) {
+      return false; // 사용자 정보가 없거나 UID가 비어 있는 경우
+    }
+    final docRef = _memberDataSource.getMemberDocumentRef(user.uid);
+
+    final docSnapshot = await docRef.get();
+    return docSnapshot.exists;
+  }
+}
