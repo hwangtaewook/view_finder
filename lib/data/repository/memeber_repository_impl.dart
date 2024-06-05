a404c5bb7d19c95506e9207306865f12b734d649
hwangtaewook <87569211+hwangtaewook@users.noreply.github.com>
2024-06-05 15:45:41 +0900

    feat : 싱글턴 패턴 추가

--- lib/data/repository/memeber_repository_impl.dart
+++ lib/data/repository/memeber_repository_impl.dart
@@ -1,8 +1,9 @@
+import 'package:injectable/injectable.dart';
 import 'package:view_finder/data/data_source/member_data_source.dart';
 import 'package:view_finder/domain/model/member.dart';
-
 import '../../domain/repository/member_repository.dart';
 
+@Singleton(as: MemberRepository)
 class MemberRepositoryImpl implements MemberRepository {
   final MemberDataSource _memberDataSource;
 
