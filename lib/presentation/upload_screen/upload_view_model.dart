9a226af2c44892596cacd640a16e99dfce416699
hwangtaewook <87569211+hwangtaewook@users.noreply.github.com>
2024-06-06 17:09:50 +0900

    feat : 업로드 뷰모델 유스케이스로 분리

--- lib/presentation/upload_screen/upload_view_model.dart
+++ lib/presentation/upload_screen/upload_view_model.dart
@@ -1,15 +1,35 @@
 import 'dart:io';
-import 'dart:typed_data';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_storage/firebase_storage.dart';
-import 'package:image/image.dart' as img;
+import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:injectable/injectable.dart';
+import 'package:view_finder/domain/use_case/upload_post_use_case.dart';
 import '../../domain/model/post.dart';
 
 @injectable
-class UploadViewModel {
+class UploadViewModel with ChangeNotifier {
+  final UploadPostUseCase _uploadPostUseCase;
+
+  UploadViewModel({
+    required UploadPostUseCase uploadPostUseCase,
+  }) : _uploadPostUseCase = uploadPostUseCase;
+
+  final Post _post = const Post(
+    postId: '',
+    userId: '',
+    userName: '',
+    userProfilePic: '',
+    title: '',
+    content: '',
+    createdAt: '',
+    imageUrl: '',
+  );
+
+  Post get post => _post;
+
+  Future<void> uploadPost(String title, String content, File imageFile) async {
+    await _uploadPostUseCase.execute(title, content, imageFile);
+  }
+
   final _picker = ImagePicker();
 
   Future<File?> getImage() async {
@@ -20,91 +40,5 @@ class UploadViewModel {
     }
 
     return File(image.path);
-  }
-
-  Future<Uint8List> _resizeAndCompressImage(File imageFile,
-      {int maxSize = 500 * 1024,
-      int maxDimension = 1280,
-      int startingQuality = 100,
-      int qualityStep = 10}) async {
-    Uint8List imageBytes = await imageFile.readAsBytes();
-
-    // 이미지를 리사이징합니다.
-    imageBytes = await _resizeImage(imageBytes, maxDimension);
-
-    int currentQuality = startingQuality;
-    int newFileSize = imageBytes.length;
-
-    // 이미지 크기가 maxSize 이하가 될 때까지 품질을 조절하면서 압축합니다.
-    while (newFileSize > maxSize && currentQuality > 0) {
-      imageBytes = await _compressImage(imageBytes, currentQuality);
-      newFileSize = imageBytes.length;
-      currentQuality -= qualityStep;
-    }
-
-    return imageBytes;
-  }
-
-  Future<Uint8List> _resizeImage(Uint8List imageBytes, int maxDimension) async {
-    img.Image image = img.decodeImage(imageBytes)!;
-
-    // 이미지의 너비 또는 높이가 maxDimension을 넘는 경우 리사이징합니다.
-    if (image.width > maxDimension || image.height > maxDimension) {
-      double aspectRatio = image.width / image.height;
-      int newWidth, newHeight;
-
-      if (image.width > image.height) {
-        newWidth = maxDimension;
-        newHeight = (maxDimension / aspectRatio).round();
-      } else {
-        newHeight = maxDimension;
-        newWidth = (maxDimension * aspectRatio).round();
-      }
-
-      image = img.copyResize(image, width: newWidth, height: newHeight);
-      imageBytes = Uint8List.fromList(img.encodeJpg(image));
-    }
-
-    return imageBytes;
-  }
-
-  Future<Uint8List> _compressImage(Uint8List imageBytes, int quality) async {
-    img.Image image = img.decodeImage(imageBytes)!;
-    return Uint8List.fromList(img.encodeJpg(image, quality: quality));
-  }
-
-  Future<void> uploadPost(String title, String content, File imageFile) async {
-    Uint8List resizedImageBytes = await _resizeAndCompressImage(imageFile);
-
-    // 이미지 업로드
-    final storageRef = FirebaseStorage.instance.ref();
-    final imageRef = storageRef
-        .child('postImages/${DateTime.now().millisecondsSinceEpoch}.jpg');
-
-    // 이미지 url 얻기
-    await imageRef.putData(resizedImageBytes);
-    final downloadUrl = await imageRef.getDownloadURL();
-
-    // 게시물 업로드
-    final postsRef =
-        // Post 데이터로 변환
-        FirebaseFirestore.instance.collection('posts').withConverter<Post>(
-              // 가져올 때 Post 형태로 변환
-              fromFirestore: (snapshot, _) => Post.fromJson(snapshot.data()!),
-              // 파이어스토어에 쓸 때는 json 형태로
-              toFirestore: (post, _) => post.toJson(),
-            );
-
-    final newPostRef = postsRef.doc();
-
-    newPostRef.set(Post(
-      postId: newPostRef.id,
-      userId: FirebaseAuth.instance.currentUser?.uid ?? '',
-      title: title,
-      content: content,
-      createdAt: DateTime.now().toString(),
-      imageUrl: downloadUrl,
-      likes: [],
-    ));
   }
 }
