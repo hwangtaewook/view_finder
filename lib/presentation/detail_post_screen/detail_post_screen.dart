9e3ebee9dd82fee26397c7b6ab8a1669e3f3376e
hwangtaewook <hwangxodnr@nate.com>
2024-06-07 18:22:10 +0900

    faet : 포스트 디테일 스크린에 바텀시트 추가

--- lib/presentation/detail_post_screen/detail_post_screen.dart
+++ lib/presentation/detail_post_screen/detail_post_screen.dart
@@ -1,5 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:view_finder/presentation/detail_post_screen/component/detail_post_app_bar.dart';
+import 'package:go_router/go_router.dart';
 import '../../domain/model/post.dart';
 
 class DetailPostScreen extends StatelessWidget {
@@ -13,19 +14,66 @@ class DetailPostScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          DetailPostAppBar(userName: post.userName),
-          SliverToBoxAdapter(
-            child: Hero(
-              tag: post.postId,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Image.network(post.imageUrl),
-                ],
-              ),
+      body: Stack(
+        children: [
+          Center(
+            child: CustomScrollView(
+              slivers: [
+                SliverFillRemaining(
+                  child: Hero(
+                    tag: post.postId,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Image.network(post.imageUrl),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          DraggableScrollableSheet(
+            snap: true,
+            initialChildSize: 0.2,
+            snapAnimationDuration: const Duration(microseconds: 10),
+            minChildSize: 0.2,
+            maxChildSize: 0.7,
+            builder: (BuildContext context, ScrollController scrollController) {
+              return Container(
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(16.0),
+                    topRight: Radius.circular(16.0),
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black26,
+                      blurRadius: 10.0,
+                      offset: Offset(0, -2),
+                    ),
+                  ],
+                ),
+                child: ListView.builder(
+                  controller: scrollController,
+                  itemCount: 25,
+                  itemBuilder: (BuildContext context, int index) {
+                    return ListTile(
+                      title: Text('Bottom Sheet Item $index'),
+                    );
+                  },
+                ),
+              );
+            },
+          ),
+          SafeArea(
+            child: IconButton(
+              onPressed: () {
+                context.pop();
+              },
+              icon: const Icon(Icons.arrow_back_ios_new),
             ),
           ),
         ],
