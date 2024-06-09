eae4205ac30b385d3b077d32cec80dd3f73bae97
hwangtaewook <87569211+hwangtaewook@users.noreply.github.com>
2024-06-09 18:25:08 +0900

    feat : 디테일 스크린 메타 데이터 추가

--- lib/presentation/detail_post_screen/detail_post_screen.dart
+++ lib/presentation/detail_post_screen/detail_post_screen.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+import 'package:view_finder/presentation/detail_post_screen/component/meta_data_item.dart';
+import 'package:view_finder/presentation/detail_post_screen/detail_post_view_model.dart';
 import '../../domain/model/post.dart';
 
 class DetailPostScreen extends StatefulWidget {
   final Post post;
 
+  @override
+  State<DetailPostScreen> createState() => _DetailPostScreenState();
+
   const DetailPostScreen({
     super.key,
     required this.post,
   });
-
-  @override
-  State<DetailPostScreen> createState() => _DetailPostScreenState();
 }
 
 class _DetailPostScreenState extends State<DetailPostScreen> {
@@ -40,7 +43,8 @@ class _DetailPostScreenState extends State<DetailPostScreen> {
 
   @override
   Widget build(BuildContext context) {
-    double imageHeight = 600; // Set this to your image height
+    double imageHeight = 0.8.sh;
+    final viewModel = context.watch<DetailPostViewModel>();
 
     return Scaffold(
       body: Stack(
@@ -115,6 +119,7 @@ class _DetailPostScreenState extends State<DetailPostScreen> {
                         child: Padding(
                           padding: EdgeInsets.symmetric(horizontal: 0.04.sw),
                           child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
                               SizedBox(
                                 height: 0.06.sh,
@@ -123,35 +128,44 @@ class _DetailPostScreenState extends State<DetailPostScreen> {
                                     ClipOval(
                                       child: Image.network(
                                         widget.post.userProfilePic,
-                                        height: 0.06.sh,
-                                        width: 0.06.sh,
+                                        height: 0.05.sh,
+                                        width: 0.05.sh,
                                         fit: BoxFit.cover,
                                       ),
                                     ),
                                     SizedBox(width: 0.05.sw),
-                                    Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        SizedBox(
-                                          height: 0.02.sh,
-                                          child: Text(
-                                            ' ${widget.post.userName}',
-                                            style: TextStyle(
-                                                fontSize: 14.sp,
-                                                color: Colors.grey),
+                                    Expanded(
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: [
+                                          SizedBox(
+                                            height: 0.023.sh,
+                                            child: Text(
+                                              overflow: TextOverflow.ellipsis,
+                                              ' ${widget.post.userNickName}',
+                                              style: TextStyle(
+                                                  fontSize: 13.sp,
+                                                  color: Colors.grey),
+                                            ),
                                           ),
-                                        ),
-                                        SizedBox(
-                                          height: 0.04.sh,
-                                          child: Text(
-                                            widget.post.title,
-                                            style: TextStyle(fontSize: 22.sp),
+                                          SizedBox(
+                                            height: 0.037.sh,
+                                            child: Text(
+                                              overflow: TextOverflow.ellipsis,
+                                              maxLines: 1,
+                                              widget.post.title,
+                                              style: TextStyle(fontSize: 18.sp),
+                                            ),
                                           ),
-                                        ),
-                                      ],
+                                        ],
+                                      ),
                                     ),
-                                    const Spacer(),
+                                    SizedBox(
+                                      width: 0.02.sw,
+                                    ),
                                     Row(
                                       children: [
                                         const Icon(Icons.favorite_border),
@@ -160,9 +174,64 @@ class _DetailPostScreenState extends State<DetailPostScreen> {
                                         ),
                                         const Icon(Icons.comment),
                                       ],
-                                    )
+                                    ),
                                   ],
                                 ),
+                              ),
+                              SizedBox(
+                                height: 0.02.sh,
+                              ),
+                              Text(
+                                widget.post.content,
+                                style: TextStyle(fontSize: 16.sp),
+                                textAlign: TextAlign.start,
+                              ),
+                              SizedBox(height: 0.02.sh),
+                              MetaDataItem(
+                                check: widget.post.metaData['Image Make'],
+                                label: '제조사',
+                                value: widget.post.metaData['Image Make'],
+                              ),
+                              MetaDataItem(
+                                check: widget.post.metaData['Image Model'],
+                                label: '카메라',
+                                value: widget.post.metaData['Image Model'],
+                              ),
+                              MetaDataItem(
+                                check: widget.post.metaData['EXIF LensModel'],
+                                label: '렌즈   ',
+                                value: widget.post.metaData['EXIF LensModel'],
+                              ),
+                              MetaDataItem(
+                                check: widget.post.metaData['Image DateTime'],
+                                label: '날짜   ',
+                                value: widget.post.metaData['Image DateTime'],
+                              ),
+                              MetaDataItem(
+                                check:
+                                    widget.post.metaData['EXIF ExposureTime'],
+                                label: '셔터    ',
+                                value:
+                                    widget.post.metaData['EXIF ExposureTime'],
+                              ),
+                              MetaDataItem(
+                                check: widget.post.metaData['EXIF FNumber'],
+                                label: '조리개',
+                                value:
+                                    'f ${viewModel.convert(widget.post.metaData['EXIF FNumber']).toString()}',
+                              ),
+                              MetaDataItem(
+                                check: widget
+                                    .post.metaData['EXIF ISOSpeedRatings'],
+                                label: 'ISO',
+                                value: widget
+                                    .post.metaData['EXIF ISOSpeedRatings'],
+                              ),
+                              MetaDataItem(
+                                check: widget.post.metaData['EXIF FocalLength'],
+                                label: '초점    ',
+                                value:
+                                    '${viewModel.convert(widget.post.metaData['EXIF FNumber']).toString()}mm',
                               ),
                             ],
                           ),
