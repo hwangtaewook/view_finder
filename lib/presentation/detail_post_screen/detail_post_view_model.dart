7dabfc31b517cf9126568c7b3d551db05f8604d4
hwangtaewook <87569211+hwangtaewook@users.noreply.github.com>
2024-06-09 18:25:32 +0900

    feat : 디테일 포스트 뷰모델에 FractionConversionService 추가

--- lib/presentation/detail_post_screen/detail_post_view_model.dart
+++ lib/presentation/detail_post_screen/detail_post_view_model.dart
@@ -2,14 +2,18 @@ import 'package:flutter/cupertino.dart';
 import 'package:injectable/injectable.dart';
 import '../../../domain/model/member.dart';
 import '../../../domain/use_case/get_member_use_case.dart';
+import '../../data/services/fraction_conversion_service.dart';
 
 @injectable
 class DetailPostViewModel with ChangeNotifier {
   final GetMemberUseCase _getMemberUseCase;
+  final FractionConversionService _fractionConversionService;
 
   DetailPostViewModel({
     required GetMemberUseCase getMemberUseCase,
-  }) : _getMemberUseCase = getMemberUseCase;
+    required FractionConversionService fractionConversionService,
+  })  : _getMemberUseCase = getMemberUseCase,
+        _fractionConversionService = fractionConversionService;
 
   Member _member = const Member(
     userId: '',
@@ -26,5 +30,9 @@ class DetailPostViewModel with ChangeNotifier {
     final member = await _getMemberUseCase.execute(uid);
     _member = member;
     notifyListeners();
+  }
+
+  num convert(String? fractionString) {
+    return _fractionConversionService.convert(fractionString);
   }
 }
