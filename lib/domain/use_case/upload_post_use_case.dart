ed5e644e84f0f0cb119c65b19aec8a32596a330d
hwangtaewook <87569211+hwangtaewook@users.noreply.github.com>
2024-06-09 18:27:18 +0900

    refactor : Post userName userNickName으로 변경 및 metaData 추가

--- lib/domain/use_case/upload_post_use_case.dart
+++ lib/domain/use_case/upload_post_use_case.dart
@@ -5,25 +5,28 @@ import 'package:flutter/cupertino.dart';
 import 'package:injectable/injectable.dart';
 import 'package:view_finder/data/services/image_upload_service.dart';
 import 'package:view_finder/domain/use_case/get_member_use_case.dart';
+import '../../data/services/image_meta_data_service.dart';
 import '../model/post.dart';
 
 @Singleton()
 class UploadPostUseCase {
   final ImageUploadService _imageUploadService;
   final GetMemberUseCase _getMemberUseCase;
+  final ImageMetaDataService _imageMetaDataService;
 
   UploadPostUseCase({
     required ImageUploadService imageUploadService,
     required GetMemberUseCase getMemberUseCase,
+    required ImageMetaDataService imageMetaDataService,
   })  : _imageUploadService = imageUploadService,
-        _getMemberUseCase = getMemberUseCase;
+        _getMemberUseCase = getMemberUseCase,
+        _imageMetaDataService = imageMetaDataService;
 
   Future<void> execute(String title, String content, File imageFile) async {
     String downloadUrl = '기본이미지';
     try {
       downloadUrl = await _imageUploadService.uploadImage(imageFile);
     } catch (e) {
-      // Handle the error if image upload fails
       debugPrint('Image upload failed: $e');
     }
 
@@ -37,19 +40,21 @@ class UploadPostUseCase {
     final newPostRef = postsRef.doc();
     final userId = FirebaseAuth.instance.currentUser?.uid ?? '';
     final getMember = await _getMemberUseCase.execute(userId);
-    final userName = getMember.userName;
+    final userNickName = getMember.userNickName;
     final userProfilePic = getMember.profilePic;
+    final metaData = await _imageMetaDataService.extractMetadata(imageFile);
 
     await newPostRef.set(Post(
       postId: newPostRef.id,
       userId: userId,
-      userName: userName,
+      userNickName: userNickName,
       userProfilePic: userProfilePic,
       title: title,
       content: content,
       createdAt: DateTime.now().toString(),
       imageUrl: downloadUrl,
       likes: [],
+      metaData: metaData,
     ));
   }
 }
