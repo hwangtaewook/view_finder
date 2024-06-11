87d05416dec9e2194bfefff6f13a6814ba5aabb6
hwangtaewook <87569211+hwangtaewook@users.noreply.github.com>
2024-06-12 03:20:49 +0900

    feat : 마이페이지 디자인 변경

--- lib/presentation/main_screen/account/account_tab.dart
+++ lib/presentation/main_screen/account/account_tab.dart
@@ -34,89 +34,110 @@ class _AccountTabState extends State<AccountTab> {
         body: CustomScrollView(
           slivers: [
             AccountAppBar(
-              userName: viewModel.member.userNickName,
+              userName: '마이페이지',
               viewModel: viewModel,
             ),
             SliverToBoxAdapter(
-              child: Stack(
-                children: [
-                  Column(
+              child: StreamBuilder<Object>(
+                stream: null,
+                builder: (context, snapshot) {
+                  return Column(
                     children: [
-                      Image.network(
-                        'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_640.jpg',
-                        height: 0.2.sh,
-                        width: 1.sw,
-                        fit: BoxFit.cover,
-                      ),
-                      Align(
-                        alignment: Alignment.centerRight,
-                        child: IconButton(
-                          iconSize: 0.03.sh,
-                          onPressed: () {},
-                          icon: const Icon(
-                            Icons.edit,
-                            // size: 0.04.sh,
-                          ),
+                      const SizedBox(height: 24),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 16),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Container(
+                              alignment: Alignment.center,
+                              height: 0.10.sh,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(0.075.sh),
+                                child: Image.network(
+                                  viewModel.member.profilePic.isEmpty
+                                      ? 'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_640.jpg'
+                                      : viewModel.member.profilePic,
+                                  height: 0.10.sh,
+                                  width: 0.10.sh,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            // SizedBox(width: 0.1.sw),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 12, vertical: 8),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  ClipRRect(
+                                    borderRadius: BorderRadius.circular(50),
+                                    child: Container(
+                                      color: Colors.grey[200],
+                                      child: const Padding(
+                                        padding: EdgeInsets.only(
+                                            right: 8,
+                                            left: 8,
+                                            top: 4,
+                                            bottom: 4),
+                                        child: Text(
+                                          '뷰파인더',
+                                          style: TextStyle(
+                                              fontSize: 10,
+                                              fontWeight: FontWeight.w400),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  const SizedBox(height: 4),
+                                  Text(
+                                    viewModel.member.userNickName,
+                                    style: const TextStyle(fontSize: 18),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            const Spacer(),
+                            Container(
+                              height: 40,
+                              width: 40,
+                              decoration: BoxDecoration(
+                                  color: Colors.grey[200],
+                                  shape: BoxShape.circle),
+                              child: IconButton(
+                                color: Colors.black54,
+                                onPressed: () {},
+                                icon: const Icon(
+                                  Icons.edit_rounded,
+                                  size: 20,
+                                ),
+                              ),
+                            )
+                          ],
                         ),
                       ),
-                      SizedBox(
-                        height: 0.03.sh,
-                      ),
-                      const Text(
-                        '뷰파인더',
-                      ),
-                      Text(
-                        viewModel.member.userName,
-                      ),
+                      SizedBox(height: 0.03.sh),
                     ],
-                  ),
-                  Column(
-                    children: [
-                      SizedBox(
-                        height: 0.05.sh,
-                      ),
-                      Container(
-                        alignment: Alignment.center,
-                        height: 0.3.sh,
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(0.075.sh),
-                          child: Image.network(
-                            viewModel.member.profilePic.isEmpty
-                                ? 'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_640.jpg'
-                                : viewModel.member.profilePic,
-                            height: 0.15.sh,
-                            width: 0.15.sh,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-                      const Divider(
-                        thickness: 5,
-                      ),
-                    ],
-                  ),
-                ],
+                  );
+                },
               ),
             ),
-            SliverPadding(
-              padding:
-                  EdgeInsets.symmetric(horizontal: 0.04.sw, vertical: 0.02.sw),
-              sliver: SliverGrid(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3, // 3열 그리드
-                  crossAxisSpacing: 0.005.sw, // 열 간격
-                  mainAxisSpacing: 0.005.sw, // 행 간격
-                  childAspectRatio: 1.0, // 너비와 높이 비율
-                ),
-                delegate: SliverChildBuilderDelegate(
-                  (BuildContext context, int index) {
-                    return Image.network(
-                      viewModel.userPost[index].imageUrl,
-                      fit: BoxFit.cover,
-                    );
-                  },
-                  childCount: viewModel.userPost.length, // 아이템 수
-                ),
+            SliverGrid(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3, // 3열 그리드
+                crossAxisSpacing: 0.005.sw, // 열 간격
+                mainAxisSpacing: 0.005.sw, // 행 간격
+                childAspectRatio: 1.0, // 너비와 높이 비율
+              ),
+              delegate: SliverChildBuilderDelegate(
+                (BuildContext context, int index) {
+                  return Image.network(
+                    viewModel.userPost[index].imageUrl,
+                    fit: BoxFit.cover,
+                  );
+                },
+                childCount: viewModel.userPost.length, // 아이템 수
               ),
             ),
             SliverToBoxAdapter(
