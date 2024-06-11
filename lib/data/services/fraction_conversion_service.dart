5cf739806515a6198f9f98013f75bf21196baebd
hwangtaewook <87569211+hwangtaewook@users.noreply.github.com>
2024-06-12 03:20:05 +0900

    fix : '/'를 포함하지 않는 String의 경우 그대로 반환하는 코드 추가

--- lib/data/services/fraction_conversion_service.dart
+++ lib/data/services/fraction_conversion_service.dart
@@ -3,10 +3,15 @@ import 'package:injectable/injectable.dart';
 @Singleton()
 class FractionConversionService {
   num convert(String? fractionString) {
-    if (fractionString == null) return 1;
+    if (fractionString == null) {
+      return 1;
+    }
+    if (!fractionString.contains('/')) {
+      return num.tryParse(fractionString) ?? 0;
+    }
     List<String> parts = fractionString.split('/');
-    int numerator = int.parse(parts[0]);
-    int denominator = int.parse(parts[1]);
+    int numerator = int.tryParse(parts[0]) ?? 0;
+    int denominator = int.tryParse(parts[1]) ?? 1;
     return numerator / denominator;
   }
 }
