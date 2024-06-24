3ec9535bcd3ac6b2d52915475dbc3e37b9a88655
hwangtaewook <87569211+hwangtaewook@users.noreply.github.com>
2024-06-24 17:54:38 +0900

    feat : calendar post model 생성

---
+++ lib/domain/model/calendar_post.dart
@@ -0,0 +1,23 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'calendar_post.freezed.dart';
+
+part 'calendar_post.g.dart';
+
+@freezed
+class CalendarPost with _$CalendarPost {
+  const factory CalendarPost({
+    required String postId,
+    required String userId,
+    required String userNickName,
+    required String title,
+    required String startDay,
+    required String endDay,
+    required String content,
+    required String color,
+    required String createdAt,
+  }) = _CalendarPost;
+
+  factory CalendarPost.fromJson(Map<String, Object?> json) =>
+      _$CalendarPostFromJson(json);
+}
