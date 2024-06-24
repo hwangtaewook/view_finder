8695ace5c0b335e3d4d96d5424f34c921263182d
hwangtaewook <87569211+hwangtaewook@users.noreply.github.com>
2024-06-24 18:12:43 +0900

    feat : 한국어 설정

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:intl/date_symbol_data_local.dart';
 import 'package:view_finder/core/router.dart';
 import 'core/di_setup.dart';
 import 'firebase_options.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,10 +13,9 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
   configureDependencies();
+  await initializeDateFormatting('ko_KR', null);
 
-  initializeDateFormatting('ko_KR', null).then((_) {
-    runApp(const MyApp());
-  });
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -25,7 +25,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     ScreenUtil.init(context);
     return MaterialApp.router(
+      locale: const Locale('ko', 'KR'),
       routerConfig: router,
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en'),
+        Locale('ko'),
+      ],
       theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
           scaffoldBackgroundColor: Colors.white,
