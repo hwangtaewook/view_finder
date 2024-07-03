cb34c4b6a82a9f55cc86e93f0816062e19e02a8d
hwangtaewook <87569211+hwangtaewook@users.noreply.github.com>
2024-07-03 18:40:23 +0900

    feat : upload announcement post 생성

---
+++ lib/domain/use_case/upload_announcement_post_use_case.dart
@@ -0,0 +1,39 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:injectable/injectable.dart';
+import 'package:view_finder/domain/model/announcement_post.dart';
+import 'get_member_use_case.dart';
+
+@Singleton()
+class UploadAnnouncementPostUseCase {
+  final GetMemberUseCase _getMemberUseCase;
+
+  UploadAnnouncementPostUseCase({
+    required GetMemberUseCase getMemberUseCase,
+  }) : _getMemberUseCase = getMemberUseCase;
+
+  Future<void> execute(String title, String content) async {
+// 게시물 업로드
+    final postsRef = FirebaseFirestore.instance
+        .collection('announcementPosts')
+        .withConverter<AnnouncementPost>(
+          fromFirestore: (snapshot, _) =>
+              AnnouncementPost.fromJson(snapshot.data()!),
+          toFirestore: (post, _) => post.toJson(),
+        );
+
+    final newPostRef = postsRef.doc();
+    final userId = FirebaseAuth.instance.currentUser?.uid ?? '';
+    final getMember = await _getMemberUseCase.execute(userId);
+    final userNickName = getMember.userNickName;
+
+    await newPostRef.set(AnnouncementPost(
+      postId: newPostRef.id,
+      userId: userId,
+      userNickName: userNickName,
+      title: title,
+      content: content,
+      createdAt: DateTime.now().toString(),
+    ));
+  }
+}
