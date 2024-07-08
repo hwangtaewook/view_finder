403aa7a5178f584dfc60d57a9a8496d2e04cd0d7
hwangtaewook <87569211+hwangtaewook@users.noreply.github.com>
2024-07-09 00:46:17 +0900

    faet : board tab 탭바 정렬

--- lib/presentation/main_screen/board/board_tab.dart
+++ lib/presentation/main_screen/board/board_tab.dart
@@ -1,11 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
-import 'package:view_finder/core/custom_app_bar.dart';
 import '../component/board_card.dart';
-import '../component/board_title.dart';
 import '../component/image_card.dart';
 import 'board_view_model.dart';
 
@@ -34,56 +32,133 @@ class _BoardTabState extends State<BoardTab> {
   @override
   Widget build(BuildContext context) {
     final viewModel = context.watch<BoardViewModel>();
+
     return SafeArea(
       child: Scaffold(
-        body: CustomScrollView(
-          slivers: [
-            CustomAppBar(
-              userImageURL: viewModel.member.profilePic.isEmpty
-                  ? 'https://cdn.pixabay.com/photo/2023/02/08/18/36/tawny-owl-7777285_640.jpg'
-                  : viewModel.member.profilePic,
-              screenName: '게시판',
-            ),
-            const BoardTitle(boardName: '공지사항'),
-            if (viewModel.announcementPost.isNotEmpty)
-              SliverList(
-                delegate: SliverChildBuilderDelegate((context, index) {
-                  return BoardCard(
-                    title: viewModel.announcementPost[index].title,
-                    content: viewModel.announcementPost[index].content,
-                  );
-                }, childCount: viewModel.announcementPost.length),
+        body: DefaultTabController(
+          length: 3,
+          child: Column(
+            children: [
+              TabBar(
+                indicatorColor: const Color(0xff355E3B),
+                unselectedLabelColor: Colors.grey,
+                labelColor: Colors.black,
+                splashFactory: NoSplash.splashFactory,
+                overlayColor: WidgetStateProperty.all(Colors.transparent),
+                tabs: const [
+                  Tab(
+                    text: '공지 게시판',
+                  ),
+                  Tab(
+                    text: '일정 게시판',
+                  ),
+                  Tab(
+                    text: '사진 게시판',
+                  ),
+                ],
               ),
-            const BoardTitle(boardName: '일정 게시판'),
-            if (viewModel.calendarPost.isNotEmpty)
-              SliverList(
-                delegate: SliverChildBuilderDelegate((context, index) {
-                  return BoardCard(
-                    title: viewModel.calendarPost[index].title,
-                    content: viewModel.calendarPost[index].content,
-                  );
-                }, childCount: viewModel.calendarPost.length),
-              ),
-            const BoardTitle(boardName: '사진 게시판'),
-            if (viewModel.post.isNotEmpty)
-              SliverList(
-                delegate: SliverChildBuilderDelegate(
-                  (context, index) {
-                    return BoardCard(
-                      title: viewModel.post[index].title,
-                      content: viewModel.post[index].content,
-                    );
-                  },
-                  childCount:
-                      viewModel.post.length > 3 ? 3 : viewModel.post.length,
+              Expanded(
+                child: TabBarView(
+                  children: [
+                    CustomScrollView(
+                      slivers: [
+                        SliverToBoxAdapter(
+                          child: SizedBox(
+                            height: 0.03.sh,
+                          ),
+                        ),
+                        if (viewModel.announcementPost.isNotEmpty)
+                          SliverList(
+                            delegate: SliverChildBuilderDelegate(
+                              (context, index) {
+                                return BoardCard(
+                                  title:
+                                      viewModel.announcementPost[index].title,
+                                  content:
+                                      viewModel.announcementPost[index].content,
+                                );
+                              },
+                              childCount: viewModel.announcementPost.length,
+                            ),
+                          ),
+                        SliverToBoxAdapter(
+                          child: SizedBox(
+                            height: 0.1.sh,
+                          ),
+                        ),
+                      ],
+                    ),
+                    CustomScrollView(
+                      slivers: [
+                        SliverToBoxAdapter(
+                          child: SizedBox(
+                            height: 0.03.sh,
+                          ),
+                        ),
+                        if (viewModel.calendarPost.isNotEmpty)
+                          SliverList(
+                            delegate: SliverChildBuilderDelegate(
+                              (context, index) {
+                                return BoardCard(
+                                  title: viewModel.calendarPost[index].title,
+                                  content:
+                                      viewModel.calendarPost[index].content,
+                                );
+                              },
+                              childCount: viewModel.calendarPost.length,
+                            ),
+                          ),
+                        SliverToBoxAdapter(
+                          child: SizedBox(
+                            height: 0.1.sh,
+                          ),
+                        ),
+                      ],
+                    ),
+                    CustomScrollView(
+                      slivers: [
+                        SliverToBoxAdapter(
+                          child: SizedBox(
+                            height: 0.03.sh,
+                          ),
+                        ),
+                        if (viewModel.post.isNotEmpty)
+                          SliverList(
+                            delegate: SliverChildBuilderDelegate(
+                              (context, index) {
+                                return GestureDetector(
+                                  onTap: () {
+                                    final post = viewModel.post[index];
+                                    context.push('/detail_post', extra: post);
+                                  },
+                                  child: ImageCard(
+                                    image: viewModel.post[index].imageUrl,
+                                    userImage:
+                                        viewModel.post[index].userProfilePic,
+                                    userNickName:
+                                        viewModel.post[index].userNickName,
+                                    function: () {
+                                      final post = viewModel.post[index];
+                                      context.push('/detail_post', extra: post);
+                                    },
+                                  ),
+                                );
+                              },
+                              childCount: viewModel.post.length,
+                            ),
+                          ),
+                        SliverToBoxAdapter(
+                          child: SizedBox(
+                            height: 0.1.sh,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
               ),
-            SliverToBoxAdapter(
-              child: SizedBox(
-                height: 0.1.sh,
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
