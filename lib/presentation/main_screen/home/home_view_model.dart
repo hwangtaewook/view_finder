ff8ecc400dabe9c5309a9b85b5e4ba36ddf5fa5d
hwangtaewook <87569211+hwangtaewook@users.noreply.github.com>
2024-07-08 10:52:25 +0900

    feat : home view model에  공지 게시판 추가

--- lib/presentation/main_screen/home/home_view_model.dart
+++ lib/presentation/main_screen/home/home_view_model.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:injectable/injectable.dart';
+import 'package:view_finder/domain/use_case/get_announcement_post_use_case.dart';
+import '../../../domain/model/announcement_post.dart';
 import '../../../domain/model/calendar_post.dart';
 import '../../../domain/model/member.dart';
 import '../../../domain/model/post.dart';
@@ -12,14 +14,16 @@ class HomeViewModel with ChangeNotifier {
   final GetPostsUseCase _getPostsUseCase;
   final GetMemberUseCase _getMemberUseCase;
   final GetCalendarPostUseCase _getCalendarPostUseCase;
-
+  final GetAnnouncementPostUseCase _getAnnouncementPostUseCase;
   HomeViewModel({
     required GetPostsUseCase getPostsUseCase,
     required GetMemberUseCase getMemberUseCase,
     required GetCalendarPostUseCase getCalendarPostUseCase,
+    required GetAnnouncementPostUseCase getAnnouncementPostUseCase,
   })  : _getPostsUseCase = getPostsUseCase,
         _getMemberUseCase = getMemberUseCase,
-        _getCalendarPostUseCase = getCalendarPostUseCase;
+        _getCalendarPostUseCase = getCalendarPostUseCase,
+        _getAnnouncementPostUseCase = getAnnouncementPostUseCase;
 
   List<Post> _post = [];
 
@@ -55,6 +59,17 @@ class HomeViewModel with ChangeNotifier {
   Future<void> setMember(String uid) async {
     final member = await _getMemberUseCase.execute(uid);
     _member = member;
+    notifyListeners();
+  }
+
+  List<AnnouncementPost> _announcementPost = [];
+
+  List<AnnouncementPost> get announcementPost =>
+      List.unmodifiable(_announcementPost);
+
+  Future<void> setAllAnnouncementPost() async {
+    final announcementPost = await _getAnnouncementPostUseCase.execute();
+    _announcementPost = announcementPost;
     notifyListeners();
   }
 }
