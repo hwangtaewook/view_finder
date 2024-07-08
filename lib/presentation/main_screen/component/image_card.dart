939b35ba8f31d58af8263a55acf1bd2194b174ff
hwangtaewook <87569211+hwangtaewook@users.noreply.github.com>
2024-07-09 00:25:22 +0900

    feat : image card 클릭 펑션 추가

--- lib/presentation/main_screen/component/image_card.dart
+++ lib/presentation/main_screen/component/image_card.dart
@@ -5,12 +5,14 @@ class ImageCard extends StatelessWidget {
   final String image;
   final String userImage;
   final String userNickName;
+  final Function() function;
 
   const ImageCard({
     super.key,
     required this.image,
     required this.userImage,
     required this.userNickName,
+    required this.function,
   });
 
   @override
@@ -28,31 +30,43 @@ class ImageCard extends StatelessWidget {
                   child: Image.network(image),
                 ),
                 Padding(
-                  padding: EdgeInsets.symmetric(vertical: 0.02.sh),
+                  padding: EdgeInsets.symmetric(vertical: 0.01.sh),
                   child: Row(
                     children: [
-                      ClipOval(
-                        child: Image.network(
-                          height: 0.08.sw,
-                          width: 0.08.sw,
-                          userImage,
-                          fit: BoxFit.cover,
+                      GestureDetector(
+                        onTap: function,
+                        child: ClipOval(
+                          child: Image.network(
+                            height: 0.07.sw,
+                            width: 0.07.sw,
+                            userImage,
+                            fit: BoxFit.cover,
+                          ),
                         ),
                       ),
                       SizedBox(
-                        width: 0.05.sw,
+                        width: 0.03.sw,
                       ),
                       Text(
                         userNickName,
-                        style: TextStyle(fontSize: 18.sp),
+                        style: TextStyle(fontSize: 14.sp),
                       ),
                       const Spacer(),
-                      const Icon(Icons.favorite_outline),
-                      const Icon(Icons.comment_outlined),
+                      const Icon(
+                        Icons.favorite_outline,
+                        size: 20,
+                      ),
+                      SizedBox(
+                        width: 0.02.sw,
+                      ),
+                      const Icon(
+                        Icons.comment_outlined,
+                        size: 20,
+                      ),
                     ],
                   ),
                 ),
-                SizedBox(height: 0.03.sh)
+                SizedBox(height: 0.02.sh)
               ],
             ),
           ],
