28f47fa6a4046b127b506f7f15e467601b01c226
hwangtaewook <87569211+hwangtaewook@users.noreply.github.com>
2024-07-09 00:44:45 +0900

    faet : custom app bar UI 수정

--- lib/core/custom_app_bar.dart
+++ lib/core/custom_app_bar.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:go_router/go_router.dart';
 
 class CustomAppBar extends StatelessWidget {
-  final String userImageURL;
   final String screenName;
 
   const CustomAppBar({
     super.key,
-    required this.userImageURL,
     required this.screenName,
   });
 
@@ -21,25 +18,6 @@ class CustomAppBar extends StatelessWidget {
       title: Text(
         screenName,
         style: TextStyle(fontSize: 18.sp),
-      ),
-      leadingWidth: 0.15.sw,
-      leading: GestureDetector(
-        onTap: () {
-          context.go('/member_info');
-        },
-        child: Padding(
-          padding: EdgeInsets.fromLTRB(0.04.sw, 0, 0, 0),
-          child: Center(
-            child: ClipOval(
-              child: Image.network(
-                height: 0.08.sw,
-                width: 0.08.sw,
-                userImageURL,
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-        ),
       ),
       pinned: true,
       scrolledUnderElevation: 0,
